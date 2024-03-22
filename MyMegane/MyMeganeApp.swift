c71535e7a1b760bd8212306b5245979c645e9994
Roman <roman.zuchowski@gmail.com>
2024-03-22 18:46:18 +0100

    Initial Commit

---
+++ MyMegane/MyMeganeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMeganeApp.swift
+//  MyMegane
+//
+//  Created by Roman Zuchowski on 22.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MyMeganeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
