f3d34b866584e739a56f6234229abb1745586d9b
Roman <roman.zuchowski@gmail.com>
2024-03-29 14:01:04 +0100

    adds additional charging modes

--- MyMegane/Models/ChargingMode.swift
+++ MyMegane/Models/ChargingMode.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
-enum ChargingMode: String {
+enum ChargingMode: String, CaseIterable {
     case immediate = "Sofort Laden"
+    case delayed = "Verzögert"
+    case planning = "Ladeplanung"
+    
+    var icon: String {
+        switch self {
+        case .immediate:
+            return "bolt.batteryblock"
+        case .delayed:
+            return "clock"
+        case .planning:
+            return "bolt.badge.clock.fill"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .immediate:
+            return "Schließen Sie das Ladekabel an Ihr Fahrzeug an, um mit dem sofortigen Ladevorgang zu beginnen."
+        case .delayed:
+            return "Beginnen Sie mit dem Aufladen Ihres Elektrofahrzeugs, wann immer Sie möchten."
+        case .planning:
+            return "Stellen Sie die wöchentliche Ladeplanung Ihres Fahrzeugs ein."
+        }
+    }
 }
